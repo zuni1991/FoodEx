450eb6b0e73e3cbdd38c7548278c3b50e2cd4727
andrewmarmolejo <35593189+andrewmarmolejo@users.noreply.github.com>
2020-03-30 12:25:15 -0700

    Login / Sign Up Complete - 03/30/2020

---
+++ FoodEx/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  FoodEx
+//
+//  Created by Andrew on 3/16/20.
+//  Copyright © 2020 Juan Zuniga. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+}
