45e4ebea769f5e05e626c8723776636b0b0e7226
ashish <avsavani@gmail.com>
2020-04-18 07:59:35 +0530

    branch

--- FoodEx/FeedViewController.swift
+++ FoodEx/FeedViewController.swift
@@ -8,15 +8,87 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 5
+        
+        query.findObjectsInBackground{(posts,error) in
+            if posts != nil{
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let  post = posts[indexPath.row]
+        
+
+        //let user = post["author"] as! PFUser
+        //cell.userName = user.username
+        
+        cell.captionLabel.text = post["caption"] as? String
+        let ImageFile = post["image"] as! PFFileObject
+        let urlString = ImageFile.url!
+        let url = URL(string: urlString)!
+        cell.photoView.af_setImage(withURL: url)
+        return cell
+
+}
+    @IBAction func Itinerario(sender: UITapGestureRecognizer) {
+        //if you need the cell or index path
+        let location = sender.location(in: self.tableView)
+        let indexPath = self.tableView.indexPathForRow(at: location)
+        if indexPath != nil
+        {
+          let a = "Ashish"
+            print("Ashish")
+          let b: String = "\n"
+          let c: String = "\n"
+          let d: String = "*Lugar Destacado"
+          let sum = a + b + c + d
+
+            let alert = UIAlertController(title: "Itinerario de Procesión", message: "\(sum)", preferredStyle: UIAlertController.Style.alert)
+            let action1 = UIAlertAction(title: "Atrás", style: UIAlertAction.Style.cancel, handler: nil)
+
+          alert.addAction(action1)
+
+
+            self.present(alert, animated: true, completion: nil);
+        }
+
+
+    }
+    
+    
+    
+
+
 
     /*
     // MARK: - Navigation
