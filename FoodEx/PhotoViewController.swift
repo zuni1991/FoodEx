711e57f88d31f4be07890ae13868f78deea8fd9f
ashish <avsavani@gmail.com>
2020-04-27 06:54:05 +0530

    Profile View Controller Data Endpoints

--- FoodEx/PhotoViewController.swift
+++ FoodEx/PhotoViewController.swift
@@ -82,19 +82,13 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
         }
         present(picker, animated: true, completion: nil)
     }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
         let size = CGSize(width: 300, height: 300)
         let scaledImage = image.af_imageScaled(to: size)
         imageView.image = scaledImage
         dismiss(animated: true, completion: nil)
-        
     }
-    
-    
-    
-    
-    
-    
     
 }
--- FoodEx/ProfileViewController.swift
+++ FoodEx/ProfileViewController.swift
@@ -10,10 +10,41 @@ import UIKit
 import AlamofireImage
 import Parse
 
-class ProfileViewController: UIViewController{
-    @IBOutlet weak var tableView: UITableView!
-    var profilePicture = [PFObject]()
-    var profileInfo = [PFObject]()
+class ProfileViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        profilePic.layer.masksToBounds = true
+        profilePic.layer.cornerRadius = profilePic.bounds.width / 2
+        
+        
+        let user = PFUser.current()
+        userName.text = user?.username
+    }
 
 
+    @IBAction func uploadPic(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        }else{
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        let image = info[.editedImage] as! UIImage
+//        let size = CGSize(width: 300, height: 300)
+//        let profilePic = image.af_imageScaled(to: size)
+//        profilePic.image = scaledImage
+//        dismiss(animated: true, completion: nil)
+//    }
 }
